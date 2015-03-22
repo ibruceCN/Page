b00a535771c39b254b88ef6d7075c95625e58153
BruceZhang <jwbzhang@thoughtworks.com>
2015-03-23 01:19:14 +0800

    refactor the view model JS

--- js/weiboModel.js
+++ js/weiboModel.js
@@ -1,48 +1,33 @@
 /**
  * Created by jwbzhang on 3/15/15.
  */
-function weiboModel() {
-    var self = this;
-    self.showAlert = ko.observable(false);
-    self.weibos = ko.observableArray([]);
-    self.textarea = ko.observable('');
-    self.alertText = ko.observable('');
 
-    self.post = function () {
-        var comments = self.textarea();
-        if (comments.length <= 0) {
-            self.showAlert(true);
-            self.alertText('You forget input some characters');
-        } else if (comments.length > 140) {
-            self.showAlert(true);
-            self.alertText('The posts should not more than 140 characters.');
-        } else {
-            self.textarea('');
-            self.alertText('');
-            self.showAlert(false);
-            self.weibos.push(newWeiboElement(self.weibos().length + 1, comments));
+var viewModel = {
+    alert : ko.observable(false),
+    weibo: {
+        description : ko.observable(''),
+        author: ko.observable('Bruce')
+    },
+    weibos : ko.observableArray([]),
+
+    close : function() {
+        this.alert(false);
+    },
+
+    post: function () {
+        if(this.weibo.description().trim().length > 0 && this.weibo.description().trim().length <= 140) {
+            var cloneModel = {
+                author: this.weibo.author(),
+                description: this.weibo.description().trim()
+            };
+            this.weibos.push(cloneModel);
+            this.alert(false);
+            this.weibo.description('');
+        }else
+        {
+            this.alert(true);
         }
-    }
-
-    this.close = function() {
-        self.showAlert(false);
-        self.alertText('');
-    }
-
-    this.enablebtn = ko.computed(function(){
-        return this.textarea().length > 0 ? "enabled":"disabled";
-    }, this);
-
-    function newWeiboElement(num, weibo_content) {
-        var des = {description: weibo_content}
-        return WeiBoInit(num, des);
-    }
-
-    function WeiBoInit(index, weibo) {
-        var self = this;
-        self.index = index;
-        self.weibo = ko.observable(weibo);
     }
 }
 
-ko.applyBindings(new weiboModel());
+ko.applyBindings(viewModel);
